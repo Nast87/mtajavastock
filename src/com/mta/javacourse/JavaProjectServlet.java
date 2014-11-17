aece400eb85246edd5b4101d8dca10ee1da46752
Nast87 <nast87@gmail.com>
2014-11-17 08:33:44 +0200

    AnastasyaZi_mtajavastock

--- src/com/mta/javacourse/JavaProjectServlet.java
+++ src/com/mta/javacourse/JavaProjectServlet.java
@@ -9,7 +9,7 @@ public class JavaProjectServlet extends HttpServlet {
 			throws IOException {
 		int num1 = 4, num2 = 3, num3 = 7;
 		int result = (num1+num2)*num3;
-		String resultStr = new String("Result of ("+num1+" + "+num2+") * "+num3+" = "+result);
+		String resultStr = new String("<h1>Result of ("+num1+" + "+num2+") * "+num3+" = "+result+"</h1>");
 		resp.setContentType("text/html");
 		resp.getWriter().println(resultStr);
 				
@@ -21,10 +21,10 @@ public class JavaProjectServlet extends HttpServlet {
 		int base = 20, exp = 13;
 		int result1 = (int)(Math.pow(base, exp));
 				
-		String line1 = new String("Calculation 1: Area of circle with radius " +radius+ " is: "+area+" square-cm");
+		String line1 = new String("Calculation 1: Area of circle with radius " +radius+ " is : "+area+" square-cm");
 		String line2 = new String("Calculation 2: Length of opposite where angle B is 30 degrees and Hypotenuse lenth is 50 cm is : "+opposite+" cm");
 		String line3 = new String("Calculation 3: Power of 20 with exp of 13 is : "+result1);
-		String resultStr1 = "<br>" + "<br>" + line1 + "<br>" + line2 + "<br>" +line3;
+		String resultStr1 = "<br>" + line1 + "<br>" + line2 + "<br>" +line3;
 				
 		resp.setContentType("text/html");
 		resp.getWriter().println(resultStr1);
